656e39fa2a66a6ec3047937e88964218514a93ec
IOEZCAN <ismail.oezcan@computacenter.com>
2022-11-25 21:34:16 +0100

    Reports added

--- src/test/java/com/testrunner/TestRunner.java
+++ src/test/java/com/testrunner/TestRunner.java
@@ -1,12 +1,15 @@
 package com.testrunner;
 
+import com.aventstack.extentreports.testng.listener.ExtentITestListenerClassAdapter;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.Listeners;
 
 @CucumberOptions(tags = "@all",
         features = {"src/test/resources/features/"},
         glue = {"com/stepdefinitions"},
         plugin =  {})
+@Listeners({ExtentITestListenerClassAdapter.class})
 public class TestRunner extends AbstractTestNGCucumberTests{
 
 }
