e9816c5c60332761be26307503659bb3b6311e82
IOEZCAN <ismail.oezcan@computacenter.com>
2022-11-30 12:41:14 +0100

    Single test report for TestNG and Cucumber testcases

--- src/test/java/com/stepdefinitions/Hooks.java
+++ src/test/java/com/stepdefinitions/Hooks.java
@@ -1,10 +1,14 @@
 package com.stepdefinitions;
 
+import com.aventstack.extentreports.service.ExtentTestManager;
 import com.microsoft.playwright.Page;
 import com.qa.opencart.factory.PlaywrightFactory;
+import com.qa.opencart.listeners.ExtentReportListener;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+import org.testng.ITestResult;
+import org.testng.internal.TestResult;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -26,12 +30,16 @@ public class Hooks {
     @After
     public void tearDown(Scenario scenario) {
         if (scenario.isFailed()) {
-            final byte[] screenshot = page.screenshot();
+            final byte[] screenshot = PlaywrightFactory.getPage().screenshot();
             scenario.attach(
                     screenshot,
                     "image/png",
                     "screenshot_" + scenario.getName() + "_" + Calendar.getInstance().getTime());
+
+            //This line is for ExtendTestManager to set screenshot into the report
+            ExtentTestManager.getTest().addScreenCaptureFromBase64String(PlaywrightFactory.takeScreenshot());
         }
+
         page.context().browser().close();
     }
 }
